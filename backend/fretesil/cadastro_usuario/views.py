bbec611201ae4d039b90511b4946f23de740a9af
EmanuelBSM <emanuelbsmartins@gmail.com>
2026-04-15 09:39:09 -0300

    views

--- backend/fretesil/cadastro_usuario/views.py
+++ backend/fretesil/cadastro_usuario/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Empresa, Caminhoneiro
+from .serializers import EmpresaSerializer, CaminhoneiroSerializer
 
-# Create your views here.
+class EmpresaViewSet(viewsets.ModelViewSet):
+    queryset = Empresa.objects.all()
+    serializer_class = EmpresaSerializer
+
+class CaminhoneiroViewSet(viewsets.ModelViewSet):
+    queryset = Caminhoneiro.objects.all() #queryset é a consulta ao banco de dados, pega todos os registros
+    serializer_class = CaminhoneiroSerializer
